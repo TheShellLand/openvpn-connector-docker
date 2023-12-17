fe5ba1fde5538ed7a82d4bd72e6e10b9ab89d07c
naisanzaa <naisanza@gmail.com>
2023-12-18 02:14:57 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,9 @@
 
 cd "$(dirname $0)"; set -xe
 
+docker rm -f openvpn-connector >/dev/null || true
 docker run -it \
+--name openvpn-connector \
 --restart always \
 --privileged \
 ghcr.io/theshellland/openvpn-connector-docker:latest  $@
